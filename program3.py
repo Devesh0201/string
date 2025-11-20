ea6f3993b8c4eaff32953df2ec86a908d6163e1d
Devesh0201 <devesh66chauhan@gmail.com>
2025-11-20 08:07:52 +0530

    Delete program3.py

--- program3.py
+++
@@ -1,7 +0,0 @@
-# print all sub array of given array
-a =[1,2,3]
-for i in range(len(a)):
-        for j in range(i,len(a)):
-            for k in range(i, j + 1):
-                print(a[k], end=" ")
-            print()
